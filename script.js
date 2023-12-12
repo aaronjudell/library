370cbf81fa1321cd4395a01d43a9c516878ec2d9
aaronjudell <ajudell@gmail.com>
2023-12-12 10:07:34 -0500

    Add functions to script.js to make Book and add to library

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const myLibrary = [];
 
-function Book() {
+function Book(author, title, pages, read) {
     // the constructor ...
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
+
+const book1 = new Book('Joe Smith', 'The Woods', 342, true);
+const book2 = new Book('Fred Flinstone', 'The Flinstones', 849, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
